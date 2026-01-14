b9a37d4b9afa6ec9a552057d2859158d1b362d83
educifuentes <edu.cifuentes@gmail.com>
2026-01-14 17:12:03 -0300

    refactor: consolidate report and tool pages into a new `pages/` directory.

--- app.py
+++ app.py
@@ -12,15 +12,15 @@ st.set_page_config(
 )
 
 # Section - Reports
-overview_page = st.Page("reports/1_overview.py", title="Overview", icon=":material/dashboard:")
-transactions_page = st.Page("reports/2_transactions.py", title="Transactions", icon=":material/inventory_2:")
-budgets_page = st.Page("reports/3_budgets.py", title="Budgets", icon=":material/inventory_2:")
+overview_page = st.Page("pages/reports/1_overview.py", title="Overview", icon=":material/dashboard:")
+transactions_page = st.Page("pages/reports/2_transactions.py", title="Transactions", icon=":material/inventory_2:")
+budgets_page = st.Page("pages/reports/3_budgets.py", title="Budgets", icon=":material/inventory_2:")
 
 
 # Section - Tools
-uploads_page = st.Page("tools/1_data_uploads.py", title="Data Uploads", icon=":material/upload_file:")
-explore_page = st.Page("tools/2_data_explorer.py", title="Data Explorer", icon=":material/search:")
-validations_page = st.Page("tools/3_data_outliers.py", title="Data Outliers", icon=":material/warning:")
+uploads_page = st.Page("pages/tools/1_data_uploads.py", title="Data Uploads", icon=":material/upload_file:")
+explore_page = st.Page("pages/tools/2_data_explorer.py", title="Data Explorer", icon=":material/search:")
+validations_page = st.Page("pages/tools/3_data_outliers.py", title="Data Outliers", icon=":material/warning:")
 
 # current page
 pg = st.navigation({
