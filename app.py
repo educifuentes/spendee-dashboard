659887c7595e947cc45530b414fc0a2611454413
educifuentes <edu.cifuentes@gmail.com>
2026-01-16 16:41:15 -0300

    temp - deactrive  auth

--- app.py
+++ app.py
@@ -33,25 +33,25 @@ pg = st.navigation({
 # ==========================================
 # Authentication
 # ==========================================
-if not st.user.get("is_logged_in"):
-    st.title("Spendee Dashboard")
-    # DEBUG
-    st.write(st.secrets)
-    try:
-        st.write("Config Options:")
-        st.write({
-            "server.enableXsrfProtection": st.get_option("server.enableXsrfProtection"),
-            "server.enableCORS": st.get_option("server.enableCORS"),
-            "server.headless": st.get_option("server.headless"),
-        })
-    except Exception as e:
-        st.write(f"Could not read config: {e}")
-    st.write(st.user)
+# if not st.user.get("is_logged_in"):
+#     st.title("Spendee Dashboard")
+#     # DEBUG
+#     st.write(st.secrets)
+#     try:
+#         st.write("Config Options:")
+#         st.write({
+#             "server.enableXsrfProtection": st.get_option("server.enableXsrfProtection"),
+#             "server.enableCORS": st.get_option("server.enableCORS"),
+#             "server.headless": st.get_option("server.headless"),
+#         })
+#     except Exception as e:
+#         st.write(f"Could not read config: {e}")
+#     st.write(st.user)
 
-    st.write("Please log in to access the dashboard.")
-    if st.button("Log in with Google", type="primary", icon=":material/login:"):
-        st.login()
-    st.stop()  # Stop execution if not logged in
+#     st.write("Please log in to access the dashboard.")
+#     if st.button("Log in with Google", type="primary", icon=":material/login:"):
+#         st.login()
+#     st.stop()  # Stop execution if not logged in
 
 # Check if user is allowed
 user_email = st.user.get("email")
