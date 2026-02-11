72e8d1c78db813145d5b8e01aab9855f5cdc3d59
educifuentes <edu.cifuentes@gmail.com>
2026-02-11 16:16:40 -0300

    fix - create_universal_amount to abs

--- utilities/data_transformations/universal_amount.py
+++ utilities/data_transformations/universal_amount.py
@@ -7,12 +7,12 @@ def create_universal_amount(df):
     If currency is USD, converts to CLP using approximate monthly rates.
     """
     if "currency" not in df.columns:
-        df["amount_universal"] = df["amount"]
+        df["amount_universal"] = df["amount"].abs()
         return df
 
     rates_map = get_usd_clp_rates_map()
 
     df["rate"] = df["date"].dt.strftime("%Y-%m").map(rates_map).fillna(900)
-    df["amount_universal_clp"] = df.apply(lambda x: x["amount"] * x["rate"] if x["currency"] == "USD" else x["amount"], axis=1).round(0)
+    df["amount_universal_clp"] = df.apply(lambda x: x["amount"] * x["rate"] if x["currency"] == "USD" else x["amount"], axis=1).round(0).abs()
 
     return df.drop(columns=["rate"])
