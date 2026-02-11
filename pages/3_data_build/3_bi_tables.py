c097bd79f5b7487ced1213d31bb4affebe297a7b
educifuentes <edu.cifuentes@gmail.com>
2026-02-11 15:35:45 -0300

    feat view bi tables

---
+++ pages/3_data_build/3_bi_tables.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+from models.marts.bi_transactions import bi_transactions
+
+from utilities.ui_components.render_model import render_model_ui
+
+st.title("Bi Tables")
+
+st.header("Bi Transactions")
+df = bi_transactions()
+render_model_ui(df)
+
